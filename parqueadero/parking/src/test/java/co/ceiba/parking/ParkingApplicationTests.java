9c7c4ae9b460fd64dcd5115ed849eade24cabd91
Wilmar Galeano Lopez - Ceiba Software <wilmar.galeano@ceiba.com.co>
2018-02-07 08:12:28 -0500

    02/07/2018

---
+++ parqueadero/parking/src/test/java/co/ceiba/parking/ParkingApplicationTests.java
@@ -0,0 +1,14 @@
+package co.ceiba.parking;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParkingApplicationTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
