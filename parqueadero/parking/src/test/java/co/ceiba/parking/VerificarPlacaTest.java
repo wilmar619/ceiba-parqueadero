55a5f62fb5ec7cd0632fb5fae2311498529cc179
Wilmar Galeano Lopez - Ceiba Software <wilmar.galeano@ceiba.com.co>
2018-02-08 15:58:13 -0500

    08/02/2017

---
+++ parqueadero/parking/src/test/java/co/ceiba/parking/VerificarPlacaTest.java
@@ -0,0 +1,33 @@
+package co.ceiba.parking;
+
+import static org.junit.Assert.assertTrue;
+import java.util.Calendar;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import co.ceiba.parking.model.VehiculoModel;
+import co.ceiba.parking.service.VigilanteService;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=ParkingApplication.class)
+
+ 
+public class VerificarPlacaTest {
+	
+	@Autowired
+	private VigilanteService vigilanteService;
+
+	@Test
+	public void TestVerificarPlacaTieneLetraAyDiaQuePuedeIngresar() {
+		VehiculoModel vehiculoModel = new VehiculoModel();
+		vehiculoModel.setPlaca("AAA123");
+		
+		boolean placaIniciaPorA = vigilanteService.verificarPlaca(vehiculoModel,Calendar.MONDAY);
+		
+		assertTrue(placaIniciaPorA);
+	}
+
+}
